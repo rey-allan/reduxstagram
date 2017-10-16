b00a23742609f30af7b2d4f03ee285d7d7884097
Allan Reyes <allan_bassail@hotmail.com>
2017-10-15 22:20:31 -0700

    Implement Single view to display a Post

--- src/components/single/Single.js
+++ src/components/single/Single.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import Post from '../post/Post';
 
-const Single = (props) => (
-  <h1>I am the Single!</h1>
+import './Single.css';
+
+const Single = ({ posts, comments, match: {params: {id}}, likePost }) => {
+  const post = retrievePostByCode(posts, id);
+  const index = findPostIndex(posts, post);
+
+  return (
+    <div className="single">
+      <Post post={post} comments={comments[post.code]} postIndex={index} likePost={likePost} />
+    </div>
+  );
+};
+
+const retrievePostByCode = (posts, code) => (
+  posts.find(post => post.code === code)
 );
+
+const findPostIndex = (posts, post) => (
+  posts.indexOf(post)
+)
+
+Single.propTypes = {
+  posts: PropTypes.array.isRequired,
+  comments: PropTypes.object.isRequired,
+  likePost: PropTypes.func.isRequired
+};
 
 export default Single;
--- src/components/single/Single.test.js
+++ src/components/single/Single.test.js
@@ -1,15 +1,78 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Single from './Single';
 
 import { mount, shallow } from 'enzyme';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
 
-it('renders without crashing', () => {
-  mount(<Single/>);
+let posts;
+let comments;
+let likePost;
+let match;
+let selectedPost;
+
+beforeAll(() => {
+  posts = [
+    {
+      code: '1',
+      caption: 'Visiting Seattle!',
+      likes: 24,
+      id: '1',
+      display_src: 'seattle.jpg'
+    },
+    {
+      code: '2',
+      caption: 'Eating @PikePlaceMarket',
+      likes: 30,
+      id: '2',
+      display_src: 'chowder.jpg'
+    }
+  ];
+
+  comments = {
+    '1': [
+      {
+        text: 'So cool!',
+        'user': 'fan123'
+      }
+    ]
+  };
+
+  likePost = jest.fn();
+
+  match = {
+    params: {
+      id: '1'
+    }
+  };
+
+  selectedPost = posts.find(post => post.code === match.params.id);
+  selectedPost.index = posts.indexOf(selectedPost);
 });
 
-it('renders a header', () => {
-  const wrapper = shallow(<Single/>);
-  const header = <h1>I am the Single!</h1>;
+it('renders without crashing', () => {
+  // Single renders a `Post` which uses Material UI components
+  // that need to be rendered inside a theme provider. It also
+  // uses ReactRouter component that need to be rendered in the
+  // context of a Router.
+  const history = createMemoryHistory();
+  mount(
+    <MuiThemeProvider>
+      <Router history={history}>
+        <Single posts={posts} comments={comments} match={match} likePost={likePost} />
+      </Router>
+    </MuiThemeProvider>
+  );
+});
 
-  expect(wrapper).toContainReact(header);
+it('renders a Post with appropriate props', () => {
+  const wrapper = shallow(<Single posts={posts} comments={comments} match={match} likePost={likePost} />);
+  const post = wrapper.find('Post');
+
+  expect(post).toBePresent();
+  expect(post).toHaveProp('post', selectedPost);
+  expect(post).toHaveProp('comments', comments[selectedPost.code]);
+  expect(post).toHaveProp('postIndex', selectedPost.index);
+  expect(post).toHaveProp('likePost');
 });
